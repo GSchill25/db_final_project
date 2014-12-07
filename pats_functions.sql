454c904e96fe59b91710ab46fbf9b6d8f2dc93f1
Graham Schilling <gschilli@andrew.cmu.edu>
2014-12-07 17:12:16 -0500

    views, functions

--- pats_functions.sql
+++ pats_functions.sql
@@ -5,14 +5,81 @@
 --
 -- calculate_total_costs
 -- (associated with two triggers: update_total_costs_for_medicines_changes & update_total_costs_for_treatments_changes)
+CREATE OR REPLACE FUNCTION update_total_costs_for_medicines_changes() RETURNS TRIGGER AS $$
+	DECLARE
+		last_visit INTEGER;
+		units_given INTEGER;
+		discount REAL;
+		cost_per_unit INTEGER;
+		medicine_cost REAL;
+		r visit_medicines%rowtype;
+	BEGIN
+		last_visit = (SELECT last_value FROM visits_id_seq);
+		FOR r in SELECT * FROM visit_medicines WHERE visit_medicines.id=last_visit LOOP
+			units_given = (SELECT units_given FROM r);
+			RAISE NOTICE '%',units_given;
+			discount = (SELECT discount FROM r);
+			RAISE NOTICE '%',discount;
+			cost_per_unit = (SELECT cost_per_unit FROM medicine_costs mc WHERE r.medicine_id=mc.medicine_id);
+			RAISE NOTICE '%',cost_per_unit;
+			medicine_cost = cost_per_unit * units_given * (1-discount);
+			RAISE NOTICE '%',medicine_cost;
+			UPDATE visits SET total_charge = (total_charge + medicine_cost) WHERE visits.id=last_visit;
+		
+		END LOOP;
+	  RETURN NULL;
 
+	END;
+	$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION update_total_costs_for_treatments_changes() RETURNS TRIGGER AS $$
+	DECLARE
+		last_visit INTEGER;
+		procedure_cost INTEGER;
+		discount REAL;
+		procedure_total REAL;
+	BEGIN
+		last_visit = (SELECT currval(pg_get_serial_sequence('visits', 'id')));
+		discount = SELECT discount FROM treaments t WHERE t.visit_id=last_visit; 
+		procedure_cost = SELECT pc.cost FROM procedure_costs pc JOIN procedures p ON pc.procedure_id=p.id
+		JOIN treaments t ON p.id=t.procedure_id WHERE t.visit_id=last_visit AND pc.end_date IS NULL;
+		procedure_total = procedure_cost * (1-discount);
+		UPDATE visits SET total_charge = (total_charge + procedure_total) WHERE visits.id=last_visit;
+	  RETURN NULL;
+
+	END;
+	$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER calculate_total_costs
+AFTER UPDATE OR INSERT ON visits
+EXECUTE PROCEDURE update_total_costs_for_medicines_changes();
 
 
 
 -- calculate_overnight_stay
 -- (associated with a trigger: update_overnight_stay_flag)
+CREATE OR REPLACE FUNCTION calculate_overnight_stay() RETURNS TRIGGER AS $$
+	DECLARE
+		last_visit INTEGER;
+		length_of_time INTEGER;
+	BEGIN
+		last_visit = OLD.id;
+		length_of_time = (SELECT SUM(p.length_of_time) FROM procedures p JOIN treatments t ON p.id=t.procedure_id
+			WHERE t.visit_id=last_visit);
+		IF (length_of_time) >= 720 THEN 
+			UPDATE visits SET overnight_stay = true WHERE id=last_visit;
+		END IF;
+	 RETURN NULL;
+	END;
+	$$ LANGUAGE plpgsql;
 
-
+CREATE TRIGGER update_overnight_stay_flag
+AFTER UPDATE ON visits
+FOR EACH ROW
+WHEN (OLD IS DISTINCT FROM NEW)
+EXECUTE PROCEDURE calculate_overnight_stay();
 
 
 -- set_end_date_for_medicine_costs
--- pats_views.sql
+++ pats_views.sql
@@ -1,5 +1,20 @@
 -- VIEWS FOR PATS DATABASE
 --
--- by (student_1) & (student_2)
+-- by Graham Schilling & Alex Mark
 --
 --
+
+
+CREATE VIEW owners_view as
+	SELECT o.first_name, o.last_name, o.street, o.city, o.state, o.zip, o.phone, o.email, o.active AS "Owner Active",
+	p.name AS "Pet", p.female, p.date_of_birth, p.active AS "Pet Active", a.name AS "Animal", v.date, v.weight, v.overnight_stay, v.total_charge
+	FROM owners o JOIN pets p ON o.id=p.owner_id LEFT JOIN visits v ON p.id=v.pet_id
+	JOIN animals a ON a.id=p.animal_id;
+
+
+CREATE VIEW medicines_view as
+	SELECT m.name AS "Medicine", m.description, m.stock_amount, m.method, m.unit, m.vaccine,
+	mc.cost_per_unit, mc.start_date, a.name, am.recommended_num_of_units
+	FROM medicines m JOIN medicine_costs mc ON m.id=mc.medicine_id JOIN animal_medicines am ON
+	m.id=am.medicine_id JOIN animals a ON am.animal_id=a.id 
+	WHERE mc.end_date IS NULL;
