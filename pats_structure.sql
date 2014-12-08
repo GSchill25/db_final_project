b167cd9978500e34855c635e684ec02effd9949d
Alex Mark <ajmark@andrew.cmu.edu>
2014-12-07 19:36:59 -0500

    not nulls

--- pats_structure.sql
+++ pats_structure.sql
@@ -5,104 +5,104 @@
 --
 
 CREATE TABLE animals (
-id SERIAL PRIMARY KEY,
-name character varying(255),
-active boolean DEFAULT true);
+id SERIAL PRIMARY KEY NOT NULL,
+name character varying(255) NOT NULL,
+active boolean DEFAULT true NOT NULL);
 
 CREATE TABLE pets(
-id SERIAL PRIMARY KEY,
-animal_id integer,
-name character varying(255),
-owner_id integer,
-female boolean,
+id SERIAL PRIMARY KEY NOT NULL,
+animal_id integer NOT NULL,
+name character varying(255) NOT NULL,
+owner_id integer NOT NULL,
+female boolean NOT NULL,
 date_of_birth date,
-active boolean DEFAULT true);
+active boolean DEFAULT true NOT NULL);
 
 CREATE TABLE owners(
-id SERIAL PRIMARY KEY,
-first_name character varying(255),
-last_name character varying(255),
-street character varying(255),
-city character varying(255),
-state character varying(255) DEFAULT 'PA',
-zip character varying(255),
+id SERIAL PRIMARY KEY NOT NULL,
+first_name character varying(255) NOT NULL,
+last_name character varying(255) NOT NULL,
+street character varying(255) NOT NULL,
+city character varying(255) NOT NULL,
+state character varying(255) DEFAULT 'PA' NOT NULL,
+zip character varying(255) NOT NULL,
 phone character varying(10),
 email character varying(255),
-active boolean DEFAULT true);
+active boolean DEFAULT true NOT NULL);
 
 CREATE TABLE visits(
-id SERIAL PRIMARY KEY,
-pet_id integer,
-date date,
+id SERIAL PRIMARY KEY NOT NULL,
+pet_id integer NOT NULL,
+date date NOT NULL,
 weight numeric,
-overnight_stay boolean DEFAULT false,
-total_charge integer);
+overnight_stay boolean DEFAULT false NOT NULL,
+total_charge integer NOT NULL);
 
 CREATE TABLE medicine_costs(
-id SERIAL PRIMARY KEY,
-medicine_id integer,
-cost_per_unit integer,
-start_date date,
+id SERIAL PRIMARY KEY NOT NULL,
+medicine_id integer NOT NULL,
+cost_per_unit integer NOT NULL,
+start_date date NOT NULL,
 end_date date);
 
 CREATE TABLE medicines(
-id SERIAL PRIMARY KEY,
-name character varying(255),
-description text,
-stock_amount integer,
-method character varying(255),
-unit character varying(255),
-vaccine boolean DEFAULT false);
+id SERIAL PRIMARY KEY NOT NULL,
+name character varying(255) NOT NULL,
+description text NOT NULL,
+stock_amount integer NOT NULL,
+method character varying(255) NOT NULL,
+unit character varying(255) NOT NULL,
+vaccine boolean DEFAULT false NOT NULL);
 
 CREATE TABLE animal_medicines(
-id SERIAL PRIMARY KEY,
-animal_id integer,
-medicine_id integer,
+id SERIAL PRIMARY KEY NOT NULL,
+animal_id integer NOT NULL,
+medicine_id integer NOT NULL,
 recommended_num_of_units numeric);
 
 CREATE TABLE visit_medicines(
-id serial PRIMARY KEY,
-visit_id integer,
-medicine_id integer,
-units_given numeric,
-discount numeric DEFAULT 0);
+id serial PRIMARY KEY NOT NULL,
+visit_id integer NOT NULL,
+medicine_id integer NOT NULL,
+units_given numeric NOT NULL,
+discount numeric DEFAULT 0 NOT NULL);
 
 CREATE TABLE notes (
-id SERIAL PRIMARY KEY,
-notable_type character varying(255),
-notable_id integer,
-title character varying(255),
-content text,
-user_id integer,
-date date);
+id SERIAL PRIMARY KEY NOT NULL,
+notable_type character varying(255) NOT NULL,
+notable_id integer NOT NULL,
+title character varying(255) NOT NULL,
+content text NOT NULL,
+user_id integer NOT NULL,
+date date NOT NULL);
 
 CREATE TABLE treatments(
-id SERIAL PRIMARY KEY,
-visit_id integer,
-procedure_id integer,
+id SERIAL PRIMARY KEY NOT NULL,
+visit_id integer NOT NULL,
+procedure_id integer NOT NULL,
 successful boolean,
-discount numeric DEFAULT 0);
+discount numeric DEFAULT 0 NOT NULL);
 
 CREATE TABLE procedures(
-id SERIAL PRIMARY KEY,
-name character varying(255),
+id SERIAL PRIMARY KEY NOT NULL,
+name character varying(255) NOT NULL,
 description text,
-length_of_time integer,
-active boolean DEFAULT true);
+length_of_time integer NOT NULL,
+active boolean DEFAULT true NOT NULL);
 
 CREATE TABLE procedure_costs(
-id SERIAL PRIMARY KEY,
-procedure_id integer,
-cost integer,
-start_date date,
+id SERIAL PRIMARY KEY NOT NULL,
+procedure_id integer NOT NULL,
+cost integer NOT NULL,
+start_date date NOT NULL,
 end_date date); 
 
 CREATE TABLE users(
-id SERIAL PRIMARY KEY,
-first_name character varying(255),
-last_name character varying(255),
-role character varying(255),
-username character varying(255) UNIQUE,
-password_digest character varying(255),
-active boolean DEFAULT true);
+id SERIAL PRIMARY KEY NOT NULL,
+first_name character varying(255) NOT NULL,
+last_name character varying(255) NOT NULL,
+role character varying(255) NOT NULL,
+username character varying(255) UNIQUE NOT NULL,
+password_digest character varying(255) NOT NULL,
+active boolean DEFAULT true NOT NULL);
 
--- pats_transaction.sql
+++ pats_transaction.sql
@@ -3,3 +3,8 @@
 -- by Alex Mark & Graham Schilling
 --
 --
+
+BEGIN;
+INSERT INTO visits (pet_id, date, weight) VALUES (173, current_date, 39);
+INSERT INTO treatments (visit_id, procedure_id, successful) VALUES (, 2, );
+COMMIT;
